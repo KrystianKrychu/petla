af6de6ced0cc3e251f733f284d48a86801fa8898
Krystian Wieczorek <krystianwieczorek70@gmail.com>
2021-12-22 14:57:02 +0100

    commit

--- src/pl/akademia108/Main.java
+++ src/pl/akademia108/Main.java
@@ -3,6 +3,16 @@ package pl.akademia108;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (short i = 0; i < 100; ++i) {
+            System.out.println("i: " + i);
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FooBar");
+            } else if (i % 3 == 0) {
+                System.out.println("Foo");
+            } else if (i % 5 == 0) {
+                System.out.println("Bar");
+            }
+            System.out.println("-------------------------");
+        }
     }
 }
